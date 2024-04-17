a31ba91a496115bf56274e27eb0e1e37f2252a5e
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-04-16 22:57:44 -0300

    Move App Module to module folder

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/module/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,7 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET - / ', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
