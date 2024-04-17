5919565774fb3b7d2b59fc64ea4c020d671fd211
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-04-16 23:08:26 -0300

    Add environment variables configuration

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvConfig } from 'src/config/env.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [getEnvConfig],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
