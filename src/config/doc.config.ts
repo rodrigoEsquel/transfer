5e215e38cbbbba62f7eb59c057e4d0a6beb2b876
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-04-17 01:50:13 -0300

    configure Swagger

---
+++ src/config/doc.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function configApiDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Transfer API')
+    .setDescription('Basic API for creating transactions')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './module/app.module';
 import { ConfigService } from '@nestjs/config';
+
+import { configApiDocumentation } from './config/doc.config';
+
+import { AppModule } from './module/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  configApiDocumentation(app);
+
   await app.listen(app.get(ConfigService).get('server.port'));
 }
 bootstrap();
