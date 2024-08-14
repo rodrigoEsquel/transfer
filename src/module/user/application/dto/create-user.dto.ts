1af7287867e3d058b30678cd16c0e67398d2353e
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-13 23:53:41 -0300

    Add DTO for user creation

---
+++ src/module/user/application/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  firstname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+}
