fdd7124c5f7a1461a8a01815021912af2bc5e7e8
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-13 23:41:44 -0300

    Update getAll users parameters on IUserRepository

--- src/module/user/application/interface/user-repository.interface.ts
+++ src/module/user/application/interface/user-repository.interface.ts
@@ -3,7 +3,7 @@ import { User } from '../../domain/user.entity';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
 
 export interface IUserRepository {
-  getAll(): Promise<User[]>;
+  getAll(page: number, limit: number): Promise<User[]>;
   getOneById(id: number): Promise<User>;
   create(user: User): Promise<User>;
   update(user: User): Promise<User>;
