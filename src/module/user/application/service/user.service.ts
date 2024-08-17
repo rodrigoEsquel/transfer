9a19a00d772d3747fa84a2f807cb6415700bf209
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-17 02:00:49 -0300

    Update User Service to return UserResponseDTO

--- src/module/user/application/service/user.service.ts
+++ src/module/user/application/service/user.service.ts
@@ -8,7 +8,7 @@ import {
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserMapper } from '../mapper/user.mapper';
-import { User } from '../../domain/user.entity';
+import { UserResponseDto } from '../dto/user-response.dto';
 
 @Injectable()
 export class UserService {
@@ -17,23 +17,30 @@ export class UserService {
     private readonly userMapper: UserMapper,
   ) {}
 
-  async getAll({ page, limit }): Promise<User[]> {
+  async getAll({ page, limit }): Promise<UserResponseDto[]> {
     limit = Math.min(limit, 100);
-    return await this.userRepository.getAll(page, limit);
+    const userList = await this.userRepository.getAll(page, limit);
+    return userList.map(this.userMapper.fromUsertoUserDto);
   }
 
-  async getOneById(id: number): Promise<User> {
-    return await this.userRepository.getOneById(id);
+  async getOneById(id: number): Promise<UserResponseDto> {
+    const user = await this.userRepository.getOneById(id);
+    return this.userMapper.fromUsertoUserDto(user);
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = this.userMapper.fromCreateUserDtoToUser(createUserDto);
-    return await this.userRepository.create(newUser);
+  async create(createUserDto: CreateUserDto): Promise<UserResponseDto> {
+    const userToUpdate = this.userMapper.fromCreateUserDtoToUser(createUserDto);
+    const newUser = await this.userRepository.create(userToUpdate);
+    return this.userMapper.fromUsertoUserDto(newUser);
   }
 
-  async update(updateUserDto: UpdateUserDto): Promise<User> {
+  async update(
+    userId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
     const userUpdate = this.userMapper.fromUpdateUserDtoToUser(updateUserDto);
-    return await this.userRepository.update(userUpdate);
+    const userUpdated = await this.userRepository.update(userId, userUpdate);
+    return this.userMapper.fromUsertoUserDto(userUpdated);
   }
 
   async delete(id: number): Promise<void> {
