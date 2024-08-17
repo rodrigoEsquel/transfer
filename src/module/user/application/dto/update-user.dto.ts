b01d168c3aa554bca27c61b3990a989938056e34
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-17 02:44:31 -0300

    Add conditional to update user DTO

--- src/module/user/application/dto/update-user.dto.ts
+++ src/module/user/application/dto/update-user.dto.ts
@@ -2,11 +2,11 @@ import { IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
-  firstName: string;
+  firstName?: string;
 
   @IsString()
-  lastName: string;
+  lastName?: string;
 
   @IsString()
-  email: string;
+  email?: string;
 }
