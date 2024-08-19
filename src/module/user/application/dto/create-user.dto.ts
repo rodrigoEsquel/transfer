5138e91b719a52e5fedc4f6205684fe31457a79f
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-18 23:24:25 -0300

    Add swagger decorator to user DTO

--- src/module/user/application/dto/create-user.dto.ts
+++ src/module/user/application/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   firstName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   lastName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
