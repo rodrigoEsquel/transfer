a4f8a34b51bcdac82c17e1c5fea70d6605b4cd5e
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-23 00:16:49 -0300

    Add Auth schema

---
+++ src/module/auth/infrastructure/database/auth.schema.ts
@@ -0,0 +1,44 @@
+import { EntitySchema } from 'typeorm';
+
+import { Auth } from '../../domain/auth.entity';
+
+export const AuthSchema = new EntitySchema<Auth>({
+  name: 'Auth',
+  target: Auth,
+  columns: {
+    id: {
+      type: Number,
+      primary: true,
+      generated: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+    },
+    secret: {
+      type: String,
+    },
+    role: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      createDate: true,
+    },
+    updatedAt: {
+      type: Date,
+      updateDate: true,
+    },
+    deletedAt: {
+      type: Date,
+      deleteDate: true,
+    },
+  },
+  relations: {
+    user: {
+      target: 'User',
+      type: 'one-to-one',
+      inverseSide: 'auth',
+    },
+  },
+});
