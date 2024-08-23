44bdda5d0af69176b0d46520d71f19f195be2f1a
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-23 00:24:05 -0300

    Add a crypto service to handling hashing

---
+++ src/module/common/application/service/crypto.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import {
+  HASH_PROVIDER,
+  IHashProvider,
+} from '../interface/hash-provider.interface';
+
+@Injectable()
+export class CryptoService {
+  constructor(
+    @Inject(HASH_PROVIDER) private readonly hashProvider: IHashProvider,
+  ) {}
+
+  async hashSecret(secret: string): Promise<string> {
+    return await this.hashProvider.hash(secret);
+  }
+
+  async verifyHash(secret: string, hash: string): Promise<boolean> {
+    return await this.verifyHash(secret, hash);
+  }
+}
