372a160b56ef2b6c166fb125b8390f572b14bd0b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-23 00:16:10 -0300

    Create token service provider

---
+++ src/module/auth/infrastructure/auth-provider/token-provider.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as jwt from 'jsonwebtoken';
+
+import { ITokenProvider } from '../../application/interface/token-provider.interface';
+import { AuthToken, AuthTokenPayload } from '../../domain/auth-tokens.entity';
+import { InvalidTokenException } from '../../application/exeption/invalid-token.exeption';
+
+@Injectable()
+export class TokenProviderService implements ITokenProvider {
+  private readonly accessTokenSecret: string;
+  private readonly refreshTokenSecret: string;
+  private readonly accessTokenDuration: string;
+  private readonly refreshTokenDuration: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.accessTokenSecret = this.configService.get('token.accessSecret');
+    this.refreshTokenSecret = this.configService.get('token.refreshSecret');
+    this.accessTokenDuration = this.configService.get(
+      'token.accessTokenDuration',
+    );
+    this.refreshTokenDuration = this.configService.get(
+      'token.refreshTokenDuration',
+    );
+  }
+
+  async generateTokens(payload: AuthTokenPayload): Promise<AuthToken> {
+    const accessToken = jwt.sign(payload, this.accessTokenSecret, {
+      expiresIn: this.accessTokenDuration,
+    });
+    const refreshToken = jwt.sign(payload, this.refreshTokenSecret, {
+      expiresIn: this.refreshTokenDuration,
+    });
+    return { accessToken, refreshToken };
+  }
+
+  async verifyAccessToken(token: string): Promise<AuthTokenPayload> {
+    try {
+      const payload = jwt.verify(
+        token,
+        this.accessTokenSecret,
+      ) as AuthTokenPayload;
+      return payload;
+    } catch (error) {
+      throw new InvalidTokenException();
+    }
+  }
+
+  async verifyRefreshToken(token: string): Promise<AuthTokenPayload> {
+    try {
+      return jwt.verify(token, this.refreshTokenSecret) as AuthTokenPayload;
+    } catch (error) {
+      throw new InvalidTokenException();
+    }
+  }
+
+  async refreshAccessToken(refreshToken: string): Promise<AuthToken> {
+    try {
+      const payload = await this.verifyRefreshToken(refreshToken);
+      delete payload.exp;
+      return this.generateTokens(payload);
+    } catch (error) {
+      throw new InvalidTokenException();
+    }
+  }
+}
