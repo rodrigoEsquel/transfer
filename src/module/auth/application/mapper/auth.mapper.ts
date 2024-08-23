a1f335e3690347c8d111a81466fc57c0b1efb977
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-23 00:27:26 -0300

    Add Auth mapping for handling DTOs

---
+++ src/module/auth/application/mapper/auth.mapper.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+import { Auth } from '../../domain/auth.entity';
+import { CreateAuthDto } from '../dto/create-auth.dto';
+import { UpdateAuthDto } from '../dto/update-auth.dto';
+
+@Injectable()
+export class AuthMapper {
+  fromCreateAuthDtoToAuth(createAuthDto: CreateAuthDto): Auth {
+    const newAuth = new Auth();
+    newAuth.username = createAuthDto.username;
+    newAuth.role = createAuthDto.role;
+    newAuth.user = createAuthDto.user;
+    return newAuth;
+  }
+
+  fromUpdateAuthDtoToAuth(updateAuthDto: UpdateAuthDto): Auth {
+    const newAuth = new Auth();
+    newAuth.username = updateAuthDto.username;
+    newAuth.role = updateAuthDto.role;
+    newAuth.user = updateAuthDto.user;
+    return newAuth;
+  }
+}
