0710e72ba2a9484d85dc6aa5aa38d4101aa0347c
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-08-27 23:45:15 -0300

    Add exception for empty secret

--- src/module/common/application/service/crypto.service.ts
+++ src/module/common/application/service/crypto.service.ts
@@ -4,6 +4,7 @@ import {
   HASH_PROVIDER,
   IHashProvider,
 } from '../interface/hash-provider.interface';
+import { EmptySecretException } from '../exception/invalid-secret.exception';
 
 @Injectable()
 export class CryptoService {
@@ -12,10 +13,16 @@ export class CryptoService {
   ) {}
 
   async hashSecret(secret: string): Promise<string> {
+    if (!secret) {
+      throw new EmptySecretException();
+    }
     return await this.hashProvider.hash(secret);
   }
 
   async verifyHash(secret: string, hash: string): Promise<boolean> {
+    if (!secret || !hash) {
+      throw new EmptySecretException();
+    }
     return await this.hashProvider.verifyHash(secret, hash);
   }
 }
