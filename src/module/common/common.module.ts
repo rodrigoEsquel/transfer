34aa84b96028ad2442a2be4202c47fb281c8e847
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-09-02 23:39:52 -0300

    Add controller for the common module

--- src/module/common/common.module.ts
+++ src/module/common/common.module.ts
@@ -4,9 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { HASH_PROVIDER } from './application/interface/hash-provider.interface';
 import { HashProviderService } from './infrastructure/hash/hash-provider.service';
 import { CryptoService } from './application/service/crypto.service';
+import { CommonController } from './interface/controller/common.controller';
 
 @Module({
   imports: [ConfigModule],
+  controllers: [CommonController],
   providers: [
     CryptoService,
     {
